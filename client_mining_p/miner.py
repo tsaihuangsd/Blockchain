63255e6c8ad833a38f70df113a04bc2cdf918ec7
tsaihuangsd <tsaihuangsd@gmail.com>
2020-02-12 21:37:38 -0800

    initial commit: pre-initial-debugging; server launched. last_block end point with errors - need to debug

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -5,7 +5,7 @@ import sys
 import json
 
 
-def proof_of_work(block):
+def proof_of_work(block_string):
     """
     Simple Proof of Work Algorithm
     Stringify the block and look for a proof.
@@ -13,7 +13,11 @@ def proof_of_work(block):
     in an effort to find a number that is a valid proof
     :return: A valid proof for the provided block
     """
-    pass
+    # block_string = json.dumps(block, sort_keys=True)
+    proof = 0
+    while valid_proof(block_string, proof) is False:
+        proof += 1
+    return proof
 
 
 def valid_proof(block_string, proof):
@@ -27,7 +31,10 @@ def valid_proof(block_string, proof):
     correct number of leading zeroes.
     :return: True if the resulting hash is a valid proof, False otherwise
     """
-    pass
+    guess = f'{block_string}{proof}'.encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+
+    return guess_hash[:6] == "000000"
 
 
 if __name__ == '__main__':
@@ -57,6 +64,7 @@ if __name__ == '__main__':
 
         # TODO: Get the block from `data` and use it to look for a new proof
         # new_proof = ???
+        new_proof = proof_of_work(data)
 
         # When found, POST it to the server {"proof": new_proof, "id": id}
         post_data = {"proof": new_proof, "id": id}
